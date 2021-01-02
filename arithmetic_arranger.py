31ba608256f7fa0b7d98457a2de283b6397d0785
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-02 14:41:50 +0700

    update

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -2,11 +2,71 @@ import re
 
 
 def arithmetic_arranger(problems, option=False):
+    firstLine = str()
+    secondLine = str()
+    thirdLine = str()
+    fourthLine = str()
+    # There should be four spaces between each problem.
+    spaceBetween = '    '
+
     if len(problems) > 5:
         return 'Error: Too many problems.'
     for p in problems:
-        if(re.search('\\*', p) or re.search('/', p)):
+        if re.search('\\*', p) or re.search('/', p):
             return "Error: Operator must be '+' or '-'."
-        if(re.search('[a-zA-Z]', p)):
+        if re.search('[a-zA-Z]', p):
             return 'Error: Numbers must only contain digits.'
-    pass
+
+        # take operands and operator from string problem
+        firstOperand = re.findall('\\d+', p)[0]
+        secondOperand = re.findall('\\d+', p)[1]
+        operator = re.findall('\\+|-', p)[0]
+        result = str(int(firstOperand) + int(secondOperand) if operator == '+' else int(
+            firstOperand) - int(secondOperand))
+
+        if len(firstOperand) > 4 or len(secondOperand) > 4:
+            return 'Error: Numbers cannot be more than four digits.'
+
+        # length of each problem
+        if len(firstOperand) >= len(secondOperand):
+            length = len(firstOperand) + 2
+        else:
+            length = len(secondOperand) + 2
+        spaceFirstLine = length - len(firstOperand)
+        spaceSecondLine = length - 2 - len(secondOperand)
+        spaceFourthLine = length - len(result)
+
+        # first line represent first operand
+        for n in range(spaceFirstLine):
+            firstLine += ' '
+        firstLine += firstOperand
+
+        # second line represent operator and second operand
+        secondLine += operator
+        secondLine += ' '
+        for n in range(spaceSecondLine):
+            secondLine += ' '
+        secondLine += secondOperand
+
+        # third line represend string '--' equal to length of each problem
+        for n in range(length):
+            thirdLine += '-'
+
+        # fourth line represend the result of each problem
+        for n in range(spaceFourthLine):
+            fourthLine += ' '
+        fourthLine += result
+
+        # add space between problem (when it not the last problem)
+        if (problems.index(p) != len(problems) - 1):
+            firstLine += spaceBetween
+            secondLine += spaceBetween
+            thirdLine += spaceBetween
+            fourthLine += spaceBetween
+
+    # config the returnstring depend option
+    if option:
+        returnString = firstLine + '\n' + secondLine + '\n' + thirdLine + '\n'+fourthLine
+    else:
+        returnString = firstLine + '\n' + secondLine + '\n' + thirdLine
+    return returnString
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from arithmetic_arranger import arithmetic_arranger
 
 print(arithmetic_arranger(
-    ["3 / 855", "3801 - 2", "45 + 43", "123 + 49"], True))
+    ["3 + 855", "9999 - 2", "45 + 43", "123 + 49", "1 - 2"], True))
